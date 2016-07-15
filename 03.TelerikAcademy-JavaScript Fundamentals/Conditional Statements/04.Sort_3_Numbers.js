0b125f5b323af5ee6d4f391c609a7f164c39d9d1
braveheart89 <braveheart1989@abv.bg>
2016-07-15 23:44:26 +0300

    update homework

--- 03.TelerikAcademy-JavaScript Fundamentals/Conditional Statements/04.Sort_3_Numbers.js
+++ 03.TelerikAcademy-JavaScript Fundamentals/Conditional Statements/04.Sort_3_Numbers.js
@@ -1,1 +1,46 @@
 "use strict";
+function sort3Numbers(arr) {
+    var firstNum = Number(arr[0]);
+    var secondNum = Number(arr[1]);
+    var thirdNum = Number(arr[2]);
+    if(firstNum == secondNum && firstNum == thirdNum) {
+        console.log(firstNum + ' ' + firstNum + ' ' + firstNum);
+        return;
+    }
+    if(firstNum > secondNum && firstNum > thirdNum) {
+        if(secondNum > thirdNum) {
+            console.log(firstNum+' '+secondNum + ' ' + thirdNum)
+        }
+        else if(thirdNum > secondNum) {
+            console.log(firstNum + ' ' + thirdNum + ' ' + secondNum)
+        }
+        else if(secondNum == thirdNum) {
+            console.log(firstNum + ' ' + secondNum + ' ' + secondNum)
+        }
+    }
+    else if(secondNum > firstNum && secondNum > thirdNum) {
+
+        if(firstNum > thirdNum) {
+            console.log(secondNum+' '+firstNum + ' ' + thirdNum)
+        }
+        else if(thirdNum > firstNum) {
+            console.log(secondNum + ' ' + thirdNum + ' ' + firstNum)
+        }
+        else if(firstNum == thirdNum) {
+            console.log(secondNum + ' ' + firstNum + ' ' + thirdNum)
+        }
+    }
+    else if(thirdNum > firstNum && thirdNum > secondNum) {
+
+        if(secondNum > firstNum) {
+            console.log(thirdNum+' '+ secondNum + ' ' + firstNum)
+        }
+        else if(firstNum > secondNum) {
+            console.log(thirdNum + ' ' + firstNum + ' ' + secondNum)
+        }
+        else if(secondNum == firstNum) {
+            console.log(thirdNum + ' ' + firstNum + ' ' + firstNum)
+        }
+    }
+}
+sort3Numbers(['-2', '-2', '1']);
--- 03.TelerikAcademy-JavaScript Fundamentals/Conditional Statements/07.The_Biggest_Of_Five_Numbers.js
+++ 03.TelerikAcademy-JavaScript Fundamentals/Conditional Statements/07.The_Biggest_Of_Five_Numbers.js
@@ -1,1 +1,24 @@
 "use strict";
+function theBiggestOfFiveNum(arr) {
+    var a = Number(arr[0]);
+    var b = Number(arr[1]);
+    var c = Number(arr[2]);
+    var d = Number(arr[3]);
+    var f = Number(arr[4]);
+    if (a > b && a > c && a > d && a > f) {
+        console.log(a);
+    }
+    else if (b > a && b > c && b > d && b > f) {
+        console.log(b);
+    }
+    else if (c > a && c > b && c > d && c > f) {
+        console.log(c);
+    }
+    else if (d > a && d > c && d > b && d > f) {
+        console.log(d);
+    }
+    else {
+        console.log(f);
+    }
+}
+theBiggestOfFiveNum(['-3', '-0.5', '-1.1', '-2', '-0.1']);
