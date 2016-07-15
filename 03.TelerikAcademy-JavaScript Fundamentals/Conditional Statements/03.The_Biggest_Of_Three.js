2bfe34a364bcf67da9892d73d7762eeefbaf8f5b
braveheart89 <braveheart1989@abv.bg>
2016-07-15 12:17:42 +0300

    homework

--- 03.TelerikAcademy-JavaScript Fundamentals/Conditional Statements/03.The_Biggest_Of_Three.js
+++ 03.TelerikAcademy-JavaScript Fundamentals/Conditional Statements/03.The_Biggest_Of_Three.js
@@ -1,1 +1,23 @@
 "use strict";
+function theBiggestOfThree(arr) {
+    var firstNum = Number(arr[0]);
+    var secondNum = Number(arr[1]);
+    var thirdNum = Number(arr[2]);
+    var greater;
+    var greatest;
+    // var maxNum = Math.max(Math.max(firstNum, secondNum, thirdNum))
+    // console.log(maxNum);
+    if (firstNum > secondNum) {
+        greater = firstNum;
+    } else {
+        greater = secondNum;
+    }
+    if (greater > thirdNum) {
+        greatest = greater;
+    } else {
+        greatest = thirdNum;
+    }
+    return console.log(greatest);
+
+}
+theBiggestOfThree(['-2', '-2', '1']);
