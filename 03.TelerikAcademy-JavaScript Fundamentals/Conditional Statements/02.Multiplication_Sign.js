cf84e5faad935675a57e85b87cbf5dd517b4c694
braveheart89 <braveheart1989@abv.bg>
2016-07-15 11:07:01 +0300

    Conditional Statements

--- 03.TelerikAcademy-JavaScript Fundamentals/Conditional Statements/02.Multiplication_Sign.js
+++ 03.TelerikAcademy-JavaScript Fundamentals/Conditional Statements/02.Multiplication_Sign.js
@@ -1,1 +1,23 @@
 "use strict";
+function multiplicationSing(arr) {
+    var firstNum= arr[0];
+    var secondNum= arr[1];
+    var thirdNum= arr[2];
+
+    if((firstNum > 0 && secondNum > 0 && thirdNum > 0)
+    ||(firstNum < 0 && secondNum < 0 && thirdNum > 0)
+        || (firstNum > 0 && secondNum < 0 && thirdNum < 0)
+        || (firstNum < 0 && secondNum > 0 && thirdNum < 0)) {
+        console.log('+')
+    }
+    else if((firstNum < 0 && secondNum > 0 && thirdNum > 0)
+        ||(firstNum > 0 && secondNum < 0 && thirdNum > 0)
+        || (firstNum > 0 && secondNum > 0 && thirdNum < 0)
+        || (firstNum < 0 && secondNum < 0 && thirdNum < 0)){
+        console.log('-')
+    }
+    else {
+        console.log('0');
+    }
+}
+multiplicationSing(['-1', '0.5', '-5.1']);
