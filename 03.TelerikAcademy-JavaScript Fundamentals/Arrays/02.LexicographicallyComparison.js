f82c2f04a77234a633dc7b17d7675fe3910edaeb
TeodorDimitrov89 <braveheart1989@abv.bg>
2017-05-01 16:07:31 +0300

    Add Practical Exams/09-July-2015 - Telerik

--- 03.TelerikAcademy-JavaScript Fundamentals/Arrays/02.LexicographicallyComparison.js
+++ 03.TelerikAcademy-JavaScript Fundamentals/Arrays/02.LexicographicallyComparison.js
@@ -1,16 +1,54 @@
 "use strict";
-function Compare(args) {
-    var str = args[0].split('\n');
-    var firstString  = str[0],
-        secondString = str[1];
+// function Compare(args) {
+//     // var str = args[0].split('\n');
+//     // var firstString  = str[0],
+//     //     secondString = str[1];
+//     //
+//     // if (firstString > secondString) {
+//     //     console.log('>');
+//     // }else if(firstString < secondString){
+//     //     console.log('<');
+//     // }
+//     // else {
+//     //     console.log('=');
+//     // }
+// }
+// Compare(['hello\nhalo']);
 
-    if (firstString > secondString) {
-        console.log('>');
-    }else if(firstString < secondString){
-        console.log('<');
+
+function solve(args) {
+    let result = args[0].split('\n');
+    let arrOne = result[0].split('');
+    let arrTwo = result[1].split('');
+
+    let smallerSize = Math.min(arrOne.length, arrTwo.length);
+    let areEqual = false;
+
+    for (let i = 0; i < smallerSize; i++) {
+        if (arrOne[i] === arrTwo[i]) {
+            areEqual = true;
+        }
+        else if (arrOne[i] > arrTwo[i]) {
+            console.log(">");
+            return;
+        }
+        else {
+            console.log("<");
+            return;
+        }
     }
-    else {
-        console.log('=');
+    //if come here then all checked symbols are same -> check for length
+    if(areEqual) {
+        if (arrOne.length > arrTwo.length) {
+            console.log(">");
+        }
+        else if (arrOne.length < arrTwo.length) {
+            console.log("<");
+        } else {
+            console.log("=");
+        }
     }
+
 }
-Compare(['hello\nhalo']);
+
+solve(['hello\nhello']);
